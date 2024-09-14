702647e864ca62e1994ed92f2fcc8260ed6d0bf1
Felipe Rodrigues <contato@feliperodrigues.net>
2024-09-13 22:11:17 -0300

    upload de novas funcionalidades

--- Assets/js/help.js
+++ Assets/js/help.js
@@ -1,3 +1,12 @@
+// Desabilitar clique direito e atalhos
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function(e) {
+    if (e.keyCode == 123 || // F12
+        (e.ctrlKey && e.shiftKey && (e.keyCode == 73 || e.keyCode == 74)) || // Ctrl+Shift+I e Ctrl+Shift+J
+        (e.ctrlKey && e.keyCode == 85)) { // Ctrl+U
+        return false;
+    }
+};
 // Abre a tela de ajuda
 
 function abrirAjuda() {
