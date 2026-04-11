8bd0001b958151bbac02f2e2b6a8c08610a34fc5
Felipe Rodrigues <65414548+lferodrigues@users.noreply.github.com>
2026-04-11 20:08:39 -0300

    Update dede.js

--- Assets/js/dede.js
+++ Assets/js/dede.js
@@ -729,8 +729,8 @@ var prompts = {
             ],
             'Criação de video VEO 3': [
                 "Primeiramente, entre na internet, estude sobre o veo 3 e crie um prompt todo em inglês para uma IA de texto para vídeo, com o seguinte conteúdo: \"{tema}\", faça com a narração em portugues BR."
-            ],
-            'novelinha': [ "crie um personagem 3D antropomorfico desse objeto estilo Disney \"{tema}\"
+            ]
+            'novelinha': [ "crie um personagem 3D antropomorfico desse objeto estilo Disney \"{tema}\"."
             ]
         };
 
